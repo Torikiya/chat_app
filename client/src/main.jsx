db8ccb97f1e7e92c14aae0daa6918e32158bfc8b
Torikiya <jhimejhimesyl@gmail.com>
2026-09-14 17:48:30 +0700

    add wrangler.json to root

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client/dist'
+import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import { GoogleOAuthProvider } from '@react-oauth/google'
 
